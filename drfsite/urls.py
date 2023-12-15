403fddb1ed86afce8b693f4e29553a6709663daa
Yerassyliitu <34080@iitu.edu.kz>
2023-12-15 15:13:16 +0600

    railway 0.6.2

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -4,27 +4,11 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 
-from testapp.views import MyLoginView, RestaurantCreateView, RestaurantListView
-from django.contrib.auth.views import LogoutView
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Your API",
-        default_version='v1',
-        description="Your API description"
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testapp.urls')),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
