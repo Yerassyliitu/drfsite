e678287f648dc50229f2ebd1fe3fc3575d093b78
Yerassyliitu <34080@iitu.edu.kz>
2023-12-15 14:54:50 +0600

    railway 0.5

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -23,7 +23,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', include('testapp.urls')),
+    path('/', include('testapp.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
